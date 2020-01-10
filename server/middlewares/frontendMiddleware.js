08179039d9de797063c256ecc765d78f3ae4e068
Alexandre BIEDIGER <alexandre.biediger@propertypassbook.com>
2020-01-10 13:42:01 +0100

    Added proxy to reddit APIa

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -6,6 +6,14 @@
 module.exports = (app, options) => {
   const isProd = process.env.NODE_ENV === 'production';
 
+  const proxy = require('http-proxy-middleware');
+  const apiProxy = proxy('/reddit', {
+    target: 'https://api.reddit.com/',
+    changeOrigin: true,
+    pathRewrite: { '^/reddit-api': '/' },
+  });
+  app.use('/reddit-api', apiProxy);
+
   if (isProd) {
     const addProdMiddlewares = require('./addProdMiddlewares');
     addProdMiddlewares(app, options);
